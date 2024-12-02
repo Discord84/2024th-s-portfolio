99d6b11923a977c1e00a798e88ba1b2001a0b65d
Discord84 <warawara2000top@gmail.com>
2024-12-02 11:35:54 +0300

    bugs fixedd

--- burger.js
+++ burger.js
@@ -6,3 +6,11 @@ burger.addEventListener("click", function(){
 burger.addEventListener("click", function(){
     burgerCont.classList.toggle("navigation-cont-active")
 })
+const burger2=document.querySelector(".burger2")
+const burgerCont2=document.querySelector(".black-navigation-cont")
+burger2.addEventListener("click", function(){
+    burger2.classList.toggle("burger2-active")
+})
+burger2.addEventListener("click", function(){
+    burgerCont2.classList.toggle("black-navigation-cont-active")
+})
